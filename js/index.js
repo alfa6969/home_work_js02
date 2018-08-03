3ffd3b2eb466144b56e7a927a3527fe0cc59430a
Tsar Andriy <tsarandrij@gmail.com>
2018-08-03 07:37:58 +0000

    Initiljs

--- js/index.js
+++ js/index.js
@@ -1,30 +1,43 @@
-//let num;
-//do {
-//    num = parseInt(prompt(''Enter namber in sequence'));
-//} while(isNaN(num));
+let num;
+do {
+    num = parseInt(prompt('Enter namber in sequence'));
+} while(isNaN(num));
 
 function fib(n) {
-  a = 1;
-  b = 1; 
-  if(n==1)
-  {
+  let a = 1;
+  let b = 1; 
+  if(n === 1) {
     alert(a);
   }
-  else if(n==2)
-  {
+  else if(n === 2) {
     alert (a + "," +b);
   }
-  else if(n>=3)
-  {
+  else if(n >= 3) {
     
     for (let i = 3; i <= n; i++) {
-    var c = a + b;
+    const c = a + b;
     a = b;
     b = c;
   }
   alert(a+ " " + b); 
-  alert(c);         
+  alert(c);
    }    
-  }
+  } document.write(c);
 
+
+
+// Лінійна
+//function fibonacci(n) {
+//  if (n <= 2)
+//  return 1;
+//  let x = 1;
+//  let y = 1;
+//  let z = 0;
+//  for (let i = 3; i < n; i++) {
+//    z = x + y;
+//    x = y;
+//    y = z;
+//  }
+//  return z;
+//}
 
