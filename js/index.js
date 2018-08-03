1b85e67bcaf804f427fa5b7f55ff7375b2610d61
Tsar Andriy <tsarandrij@gmail.com>
2018-08-03 14:00:28 +0000

    Initialjs2

--- js/index.js
+++ js/index.js
@@ -1,43 +1,39 @@
-let num;
+let n;
 do {
-    num = parseInt(prompt('Enter namber in sequence'));
-} while(isNaN(num));
+    n = parseInt(prompt('Enter namber in sequence'));
+} while(isNaN(n));
 
-function fib(n) {
-  let a = 1;
-  let b = 1; 
-  if(n === 1) {
-    alert(a);
-  }
-  else if(n === 2) {
-    alert (a + "," +b);
-  }
-  else if(n >= 3) {
-    
-    for (let i = 3; i <= n; i++) {
-    const c = a + b;
-    a = b;
-    b = c;
-  }
-  alert(a+ " " + b); 
-  alert(c);
-   }    
-  } document.write(c);
+//function fib(n) {
+//  let a = 1;
+//  let b = 1; 
+ // if(n === 1) {
+ //   alert(a);
+ // }
+ // else if(n === 2) {
+ //   alert (a + "," +b);
+ // }
+ // else if(n >= 3) {
+ //   for (let i = 3; i <= n; i++) {
+ //   const c = a + b;
+ //   a = b;
+ //   b = c;
+ // }
+ //  }    
+ // } alert (fib(n));
 
 
 
 // Лінійна
-//function fibonacci(n) {
-//  if (n <= 2)
-//  return 1;
-//  let x = 1;
-//  let y = 1;
-//  let z = 0;
-//  for (let i = 3; i < n; i++) {
-//    z = x + y;
-//    x = y;
-//    y = z;
-//  }
-//  return z;
-//}
+  let a = 1;
+  let b = 1;
+function fibonacci(n) {
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+const result = fibonacci(n);
+alert ('Namber Fibonacci' + result);
 
